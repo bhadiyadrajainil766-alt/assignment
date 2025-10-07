1e2590a3ba4438590bb627f5487bce68f01a816f
jainil bhadiyadra <bhadiyadrajainil766@gmail.com>
2025-10-07 20:19:41 +0530

    Update ass_1003_5.c

--- ass_1003_5.c
+++ ass_1003_5.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
-
-int main() {
+int main() 
+{
     int N, i;
 
     printf("Enter N: ");
     scanf("%d", &N);
 
     printf("First %d even numbers are:\n", N);
-    for(i = 1; i <= N; i++) {
+    for(i = 1; i <= N; i++) 
+    {
         printf("%d \n", 2 * i);
     }
 
