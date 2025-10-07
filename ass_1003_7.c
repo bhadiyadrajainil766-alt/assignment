3a9e26c9a710ca12ce48145e0e2ed9a4396f5190
jainil bhadiyadra <bhadiyadrajainil766@gmail.com>
2025-10-07 20:20:38 +0530

    Update ass_1003_7.c

--- ass_1003_7.c
+++ ass_1003_7.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-
-int main() {
+int main() 
+{
     int num1, num2, max, lcm;
 
     // Input two numbers
@@ -11,8 +11,10 @@ int main() {
     max = (num1 > num2) ? num1 : num2;
 
     // Find LCM
-    while (1) {
-        if (max % num1 == 0 && max % num2 == 0) {
+    while (1) 
+    {
+        if (max % num1 == 0 && max % num2 == 0) 
+        {
             lcm = max;
             break;
         }
