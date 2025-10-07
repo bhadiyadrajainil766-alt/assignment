f66c4d4ba113d3cf9b9eb164debe13eef7aba9a4
jainil bhadiyadra <bhadiyadrajainil766@gmail.com>
2025-10-07 20:19:20 +0530

    Update ass_1003_4.c

--- ass_1003_4.c
+++ ass_1003_4.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-
-int main() {
+int main() 
+{
     int n, i;
     long long product = 1;   // use long long to avoid overflow for large products
 
@@ -10,11 +10,13 @@ int main() {
     int arr[n];
 
     printf("Enter %d elements:\n", n);
-    for(i = 0; i < n; i++) {
+    for(i = 0; i < n; i++) 
+    {
         scanf("%d", &arr[i]);
     }
 
-    for(i = 0; i < n; i++) {
+    for(i = 0; i < n; i++) 
+    {
         product *= arr[i];
     }
 
