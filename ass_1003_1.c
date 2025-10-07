9e0f2749adc939cbe407c8ed32611ca21e957ab5
jainil bhadiyadra <bhadiyadrajainil766@gmail.com>
2025-10-07 20:11:42 +0530

    Update ass_1003_1.c

--- ass_1003_1.c
+++ ass_1003_1.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
-int main() {
+int main() 
+{
     char str1[100], str2[100];
     int i = 0, j = 0;
 
@@ -12,21 +13,28 @@ int main() {
     fgets(str2, sizeof(str2), stdin);
 
     // Calculate the length of str1
-    while (str1[i] != '\0') {
+    while (str1[i] != '\0') 
+    {
         i++;
     }
 
     // Calculate the length of str2
-    while (str2[j] != '\0') {
+    while (str2[j] != '\0') 
+    {
         j++;
     }
 
     // Compare lengths
-    if (i > j) {
+    if (i > j) 
+    {
         printf("The first string is longer than the second string.\n");
-    } else if (i < j) {
+    } 
+    else if (i < j) 
+    {
         printf("The second string is longer than the first string.\n");
-    } else {
+    } 
+    else 
+    {
         printf("Both strings have the same length.\n");
     }
 
