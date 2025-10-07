01658496d708c12f869a2ab5698336e7652668f3
jainil bhadiyadra <bhadiyadrajainil766@gmail.com>
2025-10-07 20:15:34 +0530

    Update ass_1003_12.c

--- ass_1003_12.c
+++ ass_1003_12.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
 
 // Function to check if number is prime
-int isPrime(int n) {
+int isPrime(int n) 
+{
     if (n < 2) return 0;
-    for (int i = 2; i <= n / 2; i++) {
+    for (int i = 2; i <= n / 2; i++) 
+    {
         if (n % i == 0) return 0;
     }
     return 1;
 }
 
 // Function to check if number is palindrome
-int isPalindrome(int n) {
+int isPalindrome(int n) 
+{
     int original = n, reversed = 0, digit;
-    while (n > 0) {
+    while (n > 0) 
+    {
         digit = n % 10;
         reversed = reversed * 10 + digit;
         n /= 10;
@@ -20,7 +24,8 @@ int isPalindrome(int n) {
     return (original == reversed);
 }
 
-int main() {
+int main() 
+{
     int num;
 
     // Input number
